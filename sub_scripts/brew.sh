e7f51c937e02840b28c434e8671a519887de6f80
Brendan McDonald <45665356+wbmcdonald4@users.noreply.github.com>
2022-10-21 21:10:03 -0400

    Update brew.sh

--- sub_scripts/brew.sh
+++ sub_scripts/brew.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 
 # Install Brew Packages
-brew install tree
 brew install pyenv
 
 # Install pyenv Build Dependencies
 brew install openssl
-brew install readline
 brew install sqlite3
 brew install xz
 brew install zlib
@@ -18,12 +16,7 @@ brew install postgresql
 brew install --cask google-chrome
 brew install --cask notion
 brew install --cask visual-studio-code
-brew install --cask zoom
 brew install --cask slack
-brew install --cask 1password
 brew install --cask ssh-tunnel-manager
-brew install --cask iterm2
-brew install --cask pgadmin4
 brew install --cask spectacle
-brew install --cask meetingbar
-brew install --cask microsoft-office
+brew install --cask spotify
